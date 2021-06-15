1eb5a856acc433273b9c6dd1d9c27fbfa9d1b9f7
Koolforkats <83036960+Koolforkats@users.noreply.github.com>
2021-06-15 20:08:08 +0100

    amended js file

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// get the current date object
+var today = new Date();
+console.log(today);
+
+
+//var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+//var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//var dateTime = date+' '+time;
