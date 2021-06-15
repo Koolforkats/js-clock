5f1654ff1a9461895ff695e54a8545b180d48fc6
Koolforkats <83036960+Koolforkats@users.noreply.github.com>
2021-06-15 20:56:26 +0100

    output clock

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,32 @@
+function outputTime() {
+    setTimeout(function() {
+
+
 // get the current date object
 var today = new Date();
-console.log(today);
+//get the current hour
+let hours = today.getHours();
+//get the current minutes
+let minutes = today.getMinutes();
+//if the length of the minuts is only 1 add a 0 first (7 = 07)
+if(minutes.length == 1){
+    minutes = '0' + minutes;
+}
+//get the current seconds
+let seconds = today.getSeconds();
+if(seconds.length == 1){
+    seconds = '0' + seconds;
+}
+//set the current time as hours, minutes and seconds
+let time = hours + ":" + minutes + ":" + seconds;
+console.log(time);
 
+let output = document.querySelector('.clockOutput');
+console.log(output);
+output.innerHTML = time;
 
-//var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-//var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-//var dateTime = date+' '+time;
+outputTime();
+    }, 250);
+}
+
+outputTime();
